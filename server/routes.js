8bde9e2e7af5421fc4eab5de073a1ae55878b910
Phan Viet Thang <thangphan@chotot.vn>
2021-03-19 16:42:58 +0700

    feat: add landing page route

--- server/routes.js
+++ server/routes.js
@@ -4,5 +4,5 @@ const nextRoutes = require('next-routes');
 // eslint-disable-next-line no-multi-assign
 const routes = (module.exports = nextRoutes());
 
-routes.add('index', '/chuong-trinh-vo-van-nha-dat');
+routes.add('index', '/chuong-trinh-nha-dat-gan-xa');
 module.exports = routes;
