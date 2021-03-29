16ecbddec20ce5c84fed3bd106f7dde39c487fb1
Phu Nguyen <phunguyen@chotot.vn>
2021-03-29 14:42:25 +0700

    fix: gateway url

--- src/containers/Home/actions.js
+++ src/containers/Home/actions.js
@@ -384,7 +384,7 @@ export const getPreview = (id) => {
   const url =
     env === 'development'
       ? `http://localhost:8080/api/v1/public/campaign/${id}`
-      : `${config.baseURL}/api/v1/public/campaign/${id}`;
+      : `${config.gatewayUrl}/api/v1/public/campaign/${id}`;
 
   return new Promise((resolve) => {
     fetch(url)
