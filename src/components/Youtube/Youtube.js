04bb5e9cf0c8a976fec881d8feb041f4d6daf14e
Duy Luong <duyluong@chotot.vn>
2022-03-21 15:14:38 +0700

    feat: update regex detect youtube link

--- src/components/Youtube/Youtube.js
+++ src/components/Youtube/Youtube.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 
 export const YoutubePreview = styled.div`
@@ -7,11 +7,33 @@ export const YoutubePreview = styled.div`
     width: 100%;
   }
 `;
+export const YoutubeEmpty = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 8px;
+`;
 
+// eslint-disable-next-line no-useless-escape
+const YOUTUBE_REGEX = /(?:https?:\/\/)?(?:www\.)?youtu\.?be(?:\.com)?\/?.*(?:watch|embed)?(?:.*v=|v\/|\/)([\w\-_]+)\&?/;
+
+// eslint-disable-next-line react/prop-types
 const Youtube = ({ embedYoutubeUrl }) => {
   const createIframeHTML = () => ({ __html: embedYoutubeUrl });
+  const [isYoutube, setIsYoutube] = useState(false);
 
-  return embedYoutubeUrl && <YoutubePreview dangerouslySetInnerHTML={createIframeHTML()} />;
+  useEffect(() => {
+    if (YOUTUBE_REGEX.test(embedYoutubeUrl)) {
+      setIsYoutube(true);
+    } else {
+      setIsYoutube(false);
+    }
+  }, [embedYoutubeUrl]);
+  return embedYoutubeUrl && isYoutube ? (
+    <YoutubePreview dangerouslySetInnerHTML={createIframeHTML()} />
+  ) : (
+    <YoutubeEmpty>Url is not a valid embed Youtube link</YoutubeEmpty>
+  );
 };
 
 export default Youtube;
